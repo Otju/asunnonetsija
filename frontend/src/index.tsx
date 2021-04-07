0c3a0881172a71c87daa323d41b88f99884f1262
Otto Söderman <ottoj.soderman@gmail.com>
2021-04-07 19:23:11 +0300

    More Heroku deployment stuff

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import { createClient, Provider } from 'urql'
 
 const client = createClient({
-  url: 'http://localhost:4000/graphql',
+  url: process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000/graphql',
 })
 
 ReactDOM.render(
